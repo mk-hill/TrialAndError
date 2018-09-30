08ca9ae55251cb4a36c8a6fb76fddd3b13529308
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-09-29 22:00:44 -0500

    Add post, put, delete methods to easy http

--- easy-http/app.js
+++ easy-http/app.js
@@ -1,9 +1,52 @@
 const http = new EasyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, postsReceived) {
+// Get posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, postsReceived) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(postsReceived);
+//   }
+// });
+
+// Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (error, post) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create data that will be used in post/put requests
+const data = {
+  title: 'Lorem ipsum',
+  body: 'Dolor sit amet',
+};
+
+// POST request
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (error, post) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// PUT request
+// http.put('https://jsonplaceholder.typicode.com/posts/59', data, function (error, post) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// DELETE request
+http.delete('https://jsonplaceholder.typicode.com/posts/59', function (error, response) {
   if (error) {
     console.log(error);
   } else {
-    console.log(postsReceived);
+    console.log(response);
   }
 });
--- easy-http/easyHttp.js
+++ easy-http/easyHttp.js
@@ -20,3 +20,39 @@ EasyHTTP.prototype.get = function (url, callback) {
   };
   this.http.send();
 };
+
+EasyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  const self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  // Send data as string
+  this.http.send(JSON.stringify(data));
+};
+
+EasyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+  const self = this;
+  this.http.onload = function () {
+    callback(null, self.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
+
+EasyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+  const self = this;
+
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, `${url} deleted`);
+    } else {
+      callback(`Error: ${self.http.status}`);
+    }
+  };
+  this.http.send();
+};
