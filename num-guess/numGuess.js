2e65030c9901aba18d8ff2dcced3a00de2be7496
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-01 14:51:28 -0500

    Wrap numGuess in iife to prevent global winningNum

--- num-guess/numGuess.js
+++ num-guess/numGuess.js
@@ -1,97 +1,102 @@
-// ? Wrapping entire code in an IIFE to prevent console exploit ?
-// ? Research further ?
+(function () {
 
-/*
-GAME FUNCTIONS:
-- Generate random min and max
-- Ensure player guess is between min and max
-- Limit number of player guesses
-- Notify player of remaining guesses
-- Notify player what the correct answer was on loss
-- Allow multiple rounds
-*/
+  // ? Wrapping entire code in an IIFE to prevent console exploit ?
+  // ? Seems to work, undo in case of issues ?
 
-/* eslint-disable */
+  /*
+  GAME FUNCTIONS:
+  - Generate random min and max
+  - Ensure player guess is between min and max
+  - Limit number of player guesses
+  - Notify player of remaining guesses
+  - Notify player what the correct answer was on loss
+  - Allow multiple rounds
+  */
 
-// Game values
-let min = 1,
-    max = 10,
-    winningNum = getWinningNum(),
-    guessesLeft = 3;
+  /* eslint-disable */
 
-// DOM Elements
-const gameEl = document.querySelector('#game'),
-      minNumEl = document.querySelector('.min-num'),
-      maxNumEl = document.querySelector('.max-num'),
-      guessBtnEl = document.querySelector('#guess-btn'),
-      guessInputEl = document.querySelector('#guess-input'),
-      messageEl = document.querySelector('.message');
+  // Game values
+  let min = 1,
+      max = 10,
+      winningNum = getWinningNum(),
+      guessesLeft = 3;
 
-// ? Use classes for stuff like this? ^
-// ? Look into reasons/best practices on eslint rule requiring separate declarations
-/* eslint-enable */
-// Might as well:
-console.log(`Here you go: ${winningNum}`);
-// Hoisting this function so we don't have to split up the variable declarations above
-function getWinningNum() {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  // DOM Elements
+  const gameEl = document.querySelector('#game'),
+        minNumEl = document.querySelector('.min-num'),
+        maxNumEl = document.querySelector('.max-num'),
+        guessBtnEl = document.querySelector('#guess-btn'),
+        guessInputEl = document.querySelector('#guess-input'),
+        messageEl = document.querySelector('.message');
 
-// Assign DOM min and max values
-minNumEl.textContent = min;
-maxNumEl.textContent = max;
-
-// Insert message into <p>
-const setMessage = (msg, color) => {
-  messageEl.style.color = color;
-  messageEl.textContent = msg;
-};
-
-// Take boolean for win state
-const gameOver = (won, msg) => {
-  const color = won ? 'green' : 'red';
-  guessInputEl.disabled = true;
-  guessInputEl.style.borderColor = color;
-  setMessage(msg, color);
-
-  // New round prompt add class for new event handler
-  guessBtnEl.value = 'Play again?';
-
-  guessBtnEl.className = 'play-again';
-};
-
-// Listen for guess
-guessBtnEl.addEventListener('click', () => {
-  const guess = parseInt(guessInputEl.value, 10);
-
-  // Validate input
-  if (!guess || guess < min || guess > max) {
-    setMessage(`Please enter a number between ${min} and ${max}.`, 'red');
-    return;
+  // ? Use classes for stuff like this? ^
+  // ? Look into reasons/best practices on eslint rule requiring separate declarations
+  /* eslint-enable */
+  // Might as well:
+  // console.log(`Here you go: ${winningNum}`);
+  // Solved with IIFE
+  // Hoisting this function so we don't have to split up the variable declarations above
+  function getWinningNum() {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
-  // Check win case
-  if (guess === winningNum) {
-    gameOver(true, `${winningNum} is correct, you win!`, 'green');
-  } else {
-    guessesLeft -= 1;
+  // Assign DOM min and max values
+  minNumEl.textContent = min;
+  maxNumEl.textContent = max;
 
-    // Check lose case
-    if (guessesLeft === 0) {
-      gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
-    } else {
-      // Game continues - wrong answer
-      guessInputEl.style.borderColor = 'red';
-      // Clear input field
-      guessInputEl.value = '';
-      setMessage(`${guess} is not correct. Remaining guesses: ${guessesLeft}`, 'red');
+  // Insert message into <p>
+  const setMessage = (msg, color) => {
+    messageEl.style.color = color;
+    messageEl.textContent = msg;
+  };
+
+  // Take boolean for win state
+  const gameOver = (won, msg) => {
+    const color = won ? 'green' : 'red';
+    guessInputEl.disabled = true;
+    guessInputEl.style.borderColor = color;
+    setMessage(msg, color);
+
+    // New round prompt add class for new event handler
+    guessBtnEl.value = 'Play again?';
+
+    guessBtnEl.className = 'play-again';
+  };
+
+  // Listen for guess
+  guessBtnEl.addEventListener('click', () => {
+    const guess = parseInt(guessInputEl.value, 10);
+
+    // Validate input
+    if (!guess || guess < min || guess > max) {
+      setMessage(`Please enter a number between ${min} and ${max}.`, 'red');
+      return;
     }
-  }
-});
 
-// Listen for new round. new-round class not present on dom load, delegating event
-gameEl.addEventListener('mousedown', (e) => {
-  if (e.target.className === 'play-again') {
-    window.location.reload();
-  }
-});
+    // Check win case
+    if (guess === winningNum) {
+      gameOver(true, `${winningNum} is correct, you win!`, 'green');
+    } else {
+      guessesLeft -= 1;
+
+      // Check lose case
+      if (guessesLeft === 0) {
+        gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
+      } else {
+        // Game continues - wrong answer
+        guessInputEl.style.borderColor = 'red';
+        // Clear input field
+        guessInputEl.value = '';
+        setMessage(`${guess} is not correct. Remaining guesses: ${guessesLeft}`, 'red');
+      }
+    }
+  });
+
+  // Listen for new round. new-round class not present on dom load, delegating event
+  gameEl.addEventListener('mousedown', (e) => {
+    if (e.target.className === 'play-again') {
+      window.location.reload();
+    }
+  });
+
+}())
