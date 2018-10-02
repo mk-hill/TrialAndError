8b1d649d32271254f66b96f108d1ec1120247f58
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-01 21:20:59 -0500

    Add greetr structure

---
+++ greetr/greetr.js
@@ -0,0 +1,14 @@
+/**
+ * Greetr
+ * When given a first name, last name, and optional language,
+ * it generates formal and informal greetings.
+ * Requirements:
+ * Support English and Spanish
+ * Be reusable
+ * Easy to type
+ * Support jQuery
+ */
+(function(global, $) {
+
+
+}(window, jQuery));
