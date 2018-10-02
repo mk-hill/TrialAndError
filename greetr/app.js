4423e8f3d6c3aeda609a31d76a15e74ad0ee7a64
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-02 11:35:46 -0500

    Add jquery compatibility for greetr dom method

--- greetr/app.js
+++ greetr/app.js
@@ -1,8 +1,17 @@
-const g = G$('John', 'Doe');
+// Testing greetr.js
+$('#login').click(function() {
+  const user = G$('John', 'Doe');
+  $('#logindiv').hide();
+  user.setLang($('#lang').val()).greetInElem('#greeting', true).log();
+});
+
+
+// Prior experiments
+// const g = G$('John', 'Doe');
 
 // g.greet();
 // g.greet(true);
 // g.log();
 
 // Chainable
-g.greet().setLang('es').greet(true).log();
+// g.greet().greetInElem('#greeting').setLang('es').log().greetInElem('#greeting', true);
--- greetr/greetr.js
+++ greetr/greetr.js
@@ -3,21 +3,27 @@
  * When given a first name, last name, and optional language,
  * it generates formal and informal greetings.
  * Requirements:
- * Support English and Spanish
- * Be reusable
- * Easy to type - G$
- * Support jQuery
+ * - Support English and Spanish
+ * - Be reusable
+ * - Easy to type - G$
+ * - Support jQuery
  */
 
-(function (global, $) {
+// ? Semicolon before iife ?
+// Can apparently be useful in case the code before has issues with
+// automatic semicolon insertion
+;(function (global, $) {
   // Point to .init for user convenience
+  // 'new' an object - added here so the user doesn't have to
   const Greetr = function (firstName, lastName, language) {
     return new Greetr.init(firstName, lastName, language);
   };
 
-  // Creating some separte variables for internal use which
-  // will not be exposed to global through Greetr object.
-  // Greetr methods will have access through closure
+  // Creating some separte variables for internal use which will be:
+  // Hidden within the scope of the IIFE,
+  // Not directly accessible,
+  // Not exposed to global through Greetr object,
+  // Accessible to Greetr methods through closure.
   const supportedLangs = ['en', 'es'];
 
   const greetings = {
@@ -35,7 +41,7 @@
     es: 'Inicio sesión',
   };
 
-  // Creating object for use below
+  // Creating object for use below after actual constructor Greetr.init
   // Prototype methods added here
   Greetr.prototype = {
     getFullName() {
@@ -45,7 +51,7 @@
     validate() {
       // ? Use .includes instead ?
       if (supportedLangs.indexOf(this.language) === -1) {
-        throw "Language not supported";
+        throw 'Language not supported';
       }
     },
 
@@ -95,11 +101,37 @@
       return this;
     },
 
+    // Insert greeting into HTML element with jQuery support
+    // Will take jQuery selector
+    greetInElem(selector, isFormal) {
+      // ? console.assert instead of these ?
+      if (!$) {
+        throw 'jQuery not loaded';
+      }
+      if (!selector) {
+        throw 'Missing jQuery selector';
+      }
+
+      let msg;
+      // Will be falsy if undefined or null
+      if (isFormal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+
+      $(selector).html(msg);
+
+      return this;
+
+    },
+
   };
 
   // Actual function constructor
+  // Object created here, allowing us to 'new' an object without calling 'new'
   Greetr.init = function (firstName = '', lastName = '', language = 'en') {
-    const self = this; // ? is this necessary without any nested functions ?
+    const self = this; // ? necessary without any nested functions ?
     // Trying defaults above instead
     // self.firstName = firstName || '';
     // self.lastName = lastName || '';
@@ -107,11 +139,14 @@
     self.firstName = firstName;
     self.lastName = lastName;
     self.language = language;
+    // Ensure unsupported language is not used when instantiating new obj
+    self.validate();
   };
 
   // Objects created with .init will use the function's prototype
   // property as their __proto__ by default.
   // Pointing to empty object created above by reference instead.
+  // Trick borrowed from jQuery so we don't have to use the 'new' keyword
   Greetr.init.prototype = Greetr.prototype;
 
   // Expose Greetr to global object passed into IIFE
@@ -119,4 +154,5 @@
   global.Greetr = global.G$ = Greetr;
 
   // Could later modify this call to check for window
+  // Passing $ in as well to allow future modification
 }(window, jQuery));
