67abf7628cfeff2a1f7ee6f807c8ded3bef21896
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-02 21:26:26 -0500

    Add user form structure

---
+++ user-form/formValidate.js
@@ -0,0 +1,4 @@
+// document.getElementById('name').addEventListener('blur', validateName);
+// document.getElementById('zip').addEventListener('blur', validateZip);
+// document.getElementById('email').addEventListener('blur', validateEmail);
+// document.getElementById('phone').addEventListener('blur', validatePhone);
