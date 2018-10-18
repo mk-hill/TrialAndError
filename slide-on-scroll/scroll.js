d7523c305bf063be1987e0f488dd260a18d7c606
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-18 17:40:18 -0500

    Update scroll.js

--- slide-on-scroll/scroll.js
+++ slide-on-scroll/scroll.js
@@ -3,7 +3,6 @@ function debounce(func, wait = 20, immediate = true) {
   return function () {
     const context = this;
 
-
     const args = arguments;
     const later = function () {
       timeout = null;
@@ -15,3 +14,24 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderImages.forEach((image) => {
+    // scrollY returns top position, we're going for when it first becomes
+    // halfway visible at the bottom of the viewport
+    const slideInAt = window.scrollY + window.innerHeight - image.height / 2;
+    // offsetTop returns pixel value of how far down the top is, adding height
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
