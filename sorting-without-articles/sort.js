76c980de3e327fc00750cfafe1b837fcbe43c955
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-25 13:07:55 -0500

    Update sorting without articles

--- sorting-without-articles/sort.js
+++ sorting-without-articles/sort.js
@@ -1,1 +1,27 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
+const articles = /^(The |A |An )/;
+
+const sortedBands = [...bands].sort(
+  (x, y) => (x.replace(articles, '') > y.replace(articles, '') ? 1 : -1),
+);
+
+const bandsEl = document.getElementById('bands');
+
+sortedBands.forEach((band) => {
+  bandsEl.innerHTML += `<li>${band}</li>`;
+});
