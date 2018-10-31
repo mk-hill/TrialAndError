98252a43842c5e1d3fc24bddcc917805e7f1597b
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-31 18:45:12 -0500

    Update speech.js

--- speech-synthesis/speech.js
+++ speech-synthesis/speech.js
@@ -1,6 +1,41 @@
 const msg = new SpeechSynthesisUtterance();
-const voices = [];
+let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  const voiceOptions = voices
+    .filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+  voicesDropdown.innerHTML = voiceOptions;
+}
+
+// Will start speaking in new voice unless false is passed in
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
