d4b57430c45a80f80858e2ffccffb766f71e273e
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-31 17:17:53 -0500

    Add speech synthesis

---
+++ speech-synthesis/speech.js
@@ -0,0 +1,6 @@
+const msg = new SpeechSynthesisUtterance();
+const voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
