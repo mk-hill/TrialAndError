bee8cee1f04a1253bb3b35ecdababfa1fcb0f6dd
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-11-05 17:19:28 -0600

    Add quicksort

--- challenges/sort.js
+++ challenges/sort.js
@@ -172,3 +172,46 @@ function merge2(left, right) {
   // console.log(left, right)
   return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
+
+//
+// ─── QUICKSORT ATTEMPT ─────────────────────────────────────────────────────────
+//
+
+function quickSort(arr, left, right) {
+  const len = arr.length;
+  let pivot;
+  let partitionIndex;
+
+  if (left < right) {
+    pivot = right;
+    partitionIndex = partition(arr, pivot, left, right);
+
+    // sort left and right
+    quickSort(arr, left, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, right);
+  }
+  return arr;
+}
+
+function partition(arr, pivot, left, right) {
+  const pivotValue = arr[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (arr[i] < pivotValue) {
+      swap(arr, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(arr, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(arr, firstIndex, secondIndex) {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+}
+
+// Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
