b3707512de897e457f3dfe30ad01037204a506e1
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-11-06 14:43:02 -0600

    Update challenges

--- challenges/recursion.js
+++ challenges/recursion.js
@@ -19,11 +19,16 @@ function findFactorialIterative(n) {
 // * the pattern of the sequence is that each value is the sum of
 // * the 2 previous values, that means that for N=5 → 2+3
 
+// ! Single calculations var for all functions !
+// ! run one at a time for accurate results !
+let calculations = 0;
+
 function fibonacciIterative(n) {
   if (n === 0 || n === 1) return n;
   let first = 0;
   let second = 1;
   for (let i = n; i > 0; i--) {
+    calculations++;
     const tempSecond = second;
     second += first;
     first = tempSecond;
@@ -34,21 +39,54 @@ function fibonacciIterative(n) {
 function fibonacciIterative2(n) {
   const arr = [0, 1];
   for (let i = 2; i <= n; i++) {
+    calculations++;
     arr.push(arr[i - 2] + arr[i - 1]);
   }
   return arr[n];
 }
 
-fibonacciIterative(5);
+// fibonacciIterative(5);
 
 // ! O(2^n) Exponential time !
 // Not to mention horribly large stack -> space
 // todo research tail call optimization
+
 function fibonacciRecursive(n) {
+  calculations++;
   return n < 2 ? n : fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-fibonacciRecursive(3);
+// fibonacciRecursive(3);
+
+// ? Dynamic programming ?
+// ? Memoization ?
+
+// Using closure for cache
+// ? Nest ternary operators for return ?
+// Leaving ifs for readability
+function memoizedFibonnaci() {
+  const cache = {};
+  return function fibRecursive(n) {
+    calculations++;
+    if (n < 2) {
+      return n;
+    }
+    if (n in cache) {
+      return cache[n];
+    }
+    cache[n] = fibRecursive(n - 1) + fibRecursive(n - 2);
+    return cache[n];
+  };
+}
+
+const fib = memoizedFibonnaci();
+
+// fibonacciIterative(10);
+// fibonacciIterative2(10);
+// fibonacciRecursive(10);
+fib(10);
+
+console.log(`${calculations} iterations/recursive function calls made.`);
 
 // * Implement a function that reverses a string using iterationand then recursion
 function reverseStringIterative(str) {
