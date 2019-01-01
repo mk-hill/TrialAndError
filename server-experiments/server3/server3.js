19553f9981bd5078b60d8ee715dd1592e3218a08
mk-hill <40850298+mk-hill@users.noreply.github.com>
2019-01-01 15:29:38 -0600

    Update server3.js

--- server-experiments/server3/server3.js
+++ server-experiments/server3/server3.js
@@ -25,3 +25,39 @@ http
   })
   .listen(59768, '127.0.0.1'); // Passing in port and IP
  */
+
+/**
+// Send json
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const obj = { first: 'John', last: 'Doe' };
+    res.end(JSON.stringify(obj));
+  })
+  .listen(59768, '127.0.0.1');
+  */
+
+http
+  .createServer((req, res) => {
+    switch (req.url) {
+      case '/':
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(`${__dirname}/index.html`).pipe(res);
+        break;
+      case '/json':
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ first: 'John', last: 'Doe' }));
+        break;
+      case '/replace':
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        let html = fs.readFileSync(`${__dirname}/index.html`, 'utf8');
+        const message = 'Replaced template text';
+        html = html.replace('{message}', message);
+        res.end(html);
+        break;
+      default:
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Nope');
+    }
+  })
+  .listen(59768, '127.0.0.1');
