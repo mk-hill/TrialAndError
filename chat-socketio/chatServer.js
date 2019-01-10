ecfdaf99230818613191aa35e40aa60a57775fd0
mk-hill <40850298+mk-hill@users.noreply.github.com>
2019-01-10 15:48:34 -0600

    Update socket.io chat server and client

--- chat-socketio/chatServer.js
+++ chat-socketio/chatServer.js
@@ -15,10 +15,10 @@ const expressServer = app.listen(59768);
   path: '/socket.io',
   serveClient: true,
 }
-* CORS rules go in "origins" property on options obj
-* Tons of other options <3 https://socket.io/docs/server-api/
-* They've covered not using "new" in the constructor as well
-* Can pass in port instead of HTTP server as well if HTTP server isn't needed
+ * CORS rules go in "origins" property on options obj
+ * Tons of other options <3 https://socket.io/docs/server-api/
+ * They've covered not using "new" in the constructor as well
+ * Can pass in port instead of HTTP server as well if HTTP server isn't needed
  */
 
 const server = new SocketIoServer(expressServer, {
@@ -26,12 +26,28 @@ const server = new SocketIoServer(expressServer, {
   serveClient: true,
 }); // Options obj can be removed, kept defaults
 
-server.on('connection', (socket) => {
+/**
+ * Client always initially connects to main namespace ("/"), can connect to others after
+ * connect fires on connection to a namespace defaults to main when namespace not specified
+ * socket that was connected on comes in as param to callback
+ * adding listeners inside cb to that socket
+ * (not actual tcp/ip socket, instance of Socket class provided defined in socket.io)
+ * Inherits from EventEmitter, overrides emit method only
+ */
+
+server.on('connect', (socket) => {
   // Custom events
-  // Emit to socket that was created on connection
-  socket.emit('msgFromServer', { data: 'Connected to socket.io server' });
-  // Listen to socket, execute callback on msgToServer event
+  // Emit to socket that was created on connection, event identified by string as expected
+  socket.emit('msgFromServer', { msg: 'Connected to socket.io server' });
+  // Register new handler for given event, inherited directly from EventEmitter
   socket.on('msgToServer', (objectFromClient) => {
     console.log(objectFromClient);
+  });
+
+  // On event from any given socket
+  socket.on('userMsg', ({ userMsg }) => {
+    // Emit event to all open sockets
+    // console.log(userMsg);
+    server.emit('msgToClients', { newMsg: userMsg });
   });
 });
--- chat-socketio/public/app.js
+++ chat-socketio/public/app.js
@@ -1,9 +1,57 @@
+//
+// ─── DOM STUFF ──────────────────────────────────────────────────────────────────
+//
 const p = document.querySelector('p');
+const msgInput = document.getElementById('user-input');
+const msgList = document.getElementById('msg-list');
 
+document.getElementById('msg-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userMsg = msgInput.value;
+  if (!userMsg) return; // ignore empty submit
+  msgInput.value = '';
+  socket.emit('userMsg', { userMsg });
+});
+
+function addMessage(msg) {
+  const newListItem = document.createElement('li');
+  newListItem.className = 'text-secondary';
+  newListItem.textContent = `${Date.now()}: ${msg}`;
+  msgList.appendChild(newListItem);
+}
+
+//
+// ─── SOCKET.IO STUFF ────────────────────────────────────────────────────────────
+//
+
+/**
+ * Client importable:
+* import io from 'socket.io-client';
+
+* Client also deliverable by server, loaded in separate script tag
+* Invocation returns "Socket" (not the same class as the one used on the server),
+* and creates new Manager
+ */
 const socket = io('http://localhost:59768');
 
+socket.on('connect', () => console.log(`Socket ID: ${socket.id}`));
+
+// Custom events work the same way here in the client (any string except reserved ones)
 socket.on('msgFromServer', (objectFromServer) => {
-  console.log(objectFromServer);
-  p.textContent = JSON.stringify(objectFromServer);
-  socket.emit('msgToServer', { data: 'Sent from client' });
+  const displayString = `Data Received: ${JSON.stringify(objectFromServer)}`;
+  console.log(displayString); // Doesn't need to be an object, just happen to be sending one
+  p.textContent = displayString;
+  socket.emit('msgToServer', { msg: 'Sent from client' });
 });
+
+/**
+ * ping/pong done automatically, defaults customizable on server
+ * Default engine.io ping/pong:
+ * pingInterval 25000 (ping every 25s)
+ * pingTimeout 5000 (wait 5s for pong packet to come back)
+ */
+
+socket.on('ping', () => console.log('Ping received from server'));
+socket.on('pong', latency => console.log(`Pong sent to server. Latency: ${latency}`));
+
+socket.on('msgToClients', ({ newMsg }) => addMessage(newMsg));
